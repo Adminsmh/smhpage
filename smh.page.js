e07ce8626e19fd451483c7d9002cb3a25e8ccb96
shangminghang <577094253@qq.com>
2019-11-16 15:36:06 +0800

    update

---
+++ smh.page.js
@@ -0,0 +1,63 @@
+function loading(obj){
+    this.Init=function(){
+        this.filling(obj.page);
+        this.eventBind();
+    };
+    this.GetIndex=function(){
+        return obj.page;
+    }
+    this.filling=function(index,t){
+        var command=obj.command;
+        obj.page=parseInt(index);
+        var start;
+        var end;
+        var html="";
+        var prev="<span class=\"prev\" data-page=\""+(index-1)+"\">上一页</span>";
+        var next="<span class=\"next\" data-page=\""+(index+1)+"\">下一页</span>";
+        if(index==1){
+            start=1;
+            end=command;
+            prev="";
+        }
+        if(index==obj.pagecount){
+            next="";
+            start=obj.pagecount-command;
+            end=obj.pagecount;
+        }
+        if(index<obj.pagecount&&(index!=1&&index!=obj.pagecount)){
+            var p = index+obj.command-1;
+            start=index;
+            var j = obj.pagecount-index+1;
+            if(j>=command){
+                end=index+command-1;
+            }else{
+                var o = command-j;
+                start=start-o;
+                end=obj.pagecount;
+            }
+        
+            start=start-1;
+ 
+
+        }
+        for(; start <= end; start++){
+            var active="";
+            if(start==index){
+                active="active";
+            }
+            html+="<span class=\""+active+"\" data-page=\""+start+"\">"+start+"</span>";
+        }
+        $("#"+obj.dom).empty().html(prev+html+next);
+    }
+    this.eventBind=function(){
+        var that = this;
+        var index = 1;
+        $("#"+obj.dom).off().on('click','span',function(){
+            index=$(this).data('page');
+            var t = $(this).attr("class");
+            console.log(t);
+            that.filling(index,t);
+            obj.callback(that);
+        })
+    }
+}
